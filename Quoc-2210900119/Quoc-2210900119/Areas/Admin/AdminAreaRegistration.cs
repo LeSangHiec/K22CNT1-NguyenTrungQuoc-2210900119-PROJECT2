3d772270f572271b45da24c37985bdb34a0e06a1
LeSangHiec <trungquock102@gmail.com>
2024-11-10 12:03:55 +0700

    quoc

---
+++ Quoc-2210900119/Quoc-2210900119/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Quoc_2210900119.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Quoc_2210900119.Areas.Admin.Controllers" }
+            );
+        }
+    }
+}
---
+++ Quoc-2210900119/Quoc-2210900119/Models/QuocSql1.Designer.cs
@@ -0,0 +1,10 @@
+﻿// T4 code generation is enabled for model 'C:\Users\Admin\Desktop\web\K22CNT1-NguyenTrungQuoc-2210900119-PROJECT2-main\Quoc-2210900119\Quoc-2210900119\Models\QuocSql1.edmx'. 
+// To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
+// property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
+// is open in the designer.
+
+// If no context and entity classes have been generated, it may be because you created an empty model but
+// have not yet chosen which version of Entity Framework to use. To generate a context class and entity
+// classes for your model, open the model in the designer, right-click on the designer surface, and
+// select 'Update Model from Database...', 'Generate Database from Model...', or 'Add Code Generation
+// Item...'.
